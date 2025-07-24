a7dff6463ef59270b5b280e6f9e181e45357ad54
denkv <denkv@users.noreply.github.com>
2025-07-24 12:15:31 +0200

    Add a list command

--- learn2rag/compose/__main__.py
+++ learn2rag/compose/__main__.py
@@ -1,14 +1,16 @@
 import argparse
+from pprint import pprint
 
 from learn2rag.compose import Project
 
 parser = argparse.ArgumentParser()
 parser.add_argument('command')
-parser.add_argument('--name', required=True)
+parser.add_argument('--name')
 parser.add_argument('--project-file')
 
 args = parser.parse_args()
 if args.command == 'create':
+    assert args.name is not None
     assert args.project_file is not None
     proj = Project.create(args.project_file, args.name)
     if proj is not None:
@@ -16,10 +18,15 @@ if args.command == 'create':
     else:
         print('Failed')
 elif args.command == 'start':
+    assert args.name is not None
     Project.get(args.name).start()
 elif args.command == 'stop':
+    assert args.name is not None
     Project.get(args.name).stop()
 elif args.command == 'remove':
+    assert args.name is not None
     Project.get(args.name).remove()
+elif args.command == 'list':
+    pprint(Project.get_all())
 else:
     raise AssertionError('Unknown command')
