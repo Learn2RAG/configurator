997d9b50571509cd3690444af6d823cf6c6bb4da
denkv <denkv@users.noreply.github.com>
2025-07-28 11:34:27 +0200

    Multiple import directories

--- importer.py
+++ importer.py
@@ -4,7 +4,11 @@ import sys
 import main
 
 main.vector_store = main.RedisVectorStore(main.embeddings, os.environ.get('REDIS_INDEX_NAME'))
-main.load(os.environ.get('IMPORT_DIR'))
+import_dirs = os.environ.get('IMPORT_DIRS')
+print(f'{import_dirs=}')
+for import_dir in import_dirs.split('|'):
+    main.load(import_dir)
+
 main.index()
 print('Importer done')
 sys.exit(0)
