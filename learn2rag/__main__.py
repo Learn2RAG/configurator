63b6e514a3119d7c0076ef6f1f21b0ded57c7590
denkv <denkv@users.noreply.github.com>
2025-09-02 14:25:20 +0200

    Formatting

--- learn2rag/__main__.py
+++ learn2rag/__main__.py
@@ -1,2 +1,8 @@
 import uvicorn
-uvicorn.run('learn2rag.ui:create_app', interface='wsgi', factory=True, host='0.0.0.0', port=9000)
+uvicorn.run(
+    'learn2rag.ui:create_app',
+    factory=True,
+    interface='wsgi',
+    host='0.0.0.0',  # TODO
+    port=9000,  # TODO
+)
