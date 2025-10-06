d324c5effacec03f6d2410bdb38baf28713ad4d9
denkv <denkv@users.noreply.github.com>
2025-10-06 11:10:21 +0200

    Display URL and open the web browser on start

--- learn2rag/__main__.py
+++ learn2rag/__main__.py
@@ -1,18 +1,48 @@
+import platform
+import subprocess
+import sys
+
 import uvicorn
 import yaml
-import learn2rag.ui
 
-config = {}
-try:
-    config = yaml.safe_load(open('config.yml'))
-except FileNotFoundError:
-    pass
 
-app = learn2rag.ui.create_app(config=config)
+def webbrowser_open(url):
+    try:
+        if platform.system() == 'Windows':
+            subprocess.Popen(['explorer', url])
+        else:
+            subprocess.Popen(['xdg-open', url])
+    except FileNotFoundError:
+        pass
+    except Exception as e:
+        print(e)
 
-uvicorn.run(
-    app,
-    interface='wsgi',
-    host=config.get('host', '0.0.0.0'),
-    port=int(config.get('port', 9000)),
-)
+
+def start_ui(config):
+    from learn2rag.ui import create_app
+    app = create_app(config=config)
+
+    port = config.get('port', '9000')
+
+    url = 'http://localhost:' + port
+    webbrowser_open(url)
+    print('*' * 40)
+    print('Learn2RAG: ' + url)
+    print('*' * 40)
+
+    uvicorn.run(
+        app,
+        interface='wsgi',
+        host=config.get('host', '0.0.0.0'),
+        port=int(port),
+    )
+
+
+if __name__ == '__main__':
+    config = {}
+    try:
+        config = yaml.safe_load(open('config.yml'))
+    except FileNotFoundError:
+        pass
+
+    start_ui(config)
