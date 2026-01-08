b158e11aac23cbb275eb0b4818089be560816b75
denkv <denkv@users.noreply.github.com>
2026-01-08 10:08:19 +0100

    Add dataset downloader

---
+++ learn2rag/evaluation/downloader.py
@@ -0,0 +1,8 @@
+from datasets import load_dataset
+
+
+load_dataset('ServiceNow/repliqa').save_to_disk('datasets/repliqa/source')
+load_dataset('rag-datasets/rag-mini-bioasq', 'text-corpus').save_to_disk('datasets/rag-mini-bioasq/source/text-corpus')
+load_dataset('rag-datasets/rag-mini-bioasq', 'question-answer-passages').save_to_disk('datasets/rag-mini-bioasq/source/question-answer-passages')
+load_dataset('hotpotqa/hotpot_qa', 'distractor').save_to_disk('datasets/hotpot_qa/source/distractor')
+load_dataset('hotpotqa/hotpot_qa', 'fullwiki').save_to_disk('datasets/hotpot_qa/source/fullwiki')
