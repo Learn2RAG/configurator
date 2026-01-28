57d425a1652926647825f1961b86bcac2531fa53
denkv <denkv@users.noreply.github.com>
2026-01-28 14:15:15 +0100

    Add unit test structure

---
+++ learn2rag/pipeline/tests/test_misc.py
@@ -0,0 +1,16 @@
+import unittest
+
+from ..embeddings import create_embeddings
+
+
+class PipelineMiscTestCase(unittest.TestCase):
+    def test_embeddings(self):
+        input_sample = [
+            'Paderborn is a city in eastern North Rhine-Westphalia, Germany, capital of the Paderborn district.',
+            'Bielefeld is a city in the Ostwestfalen-Lippe Region in the north-east of North Rhine-Westphalia, Germany.',
+        ]
+        model_name = 'sentence-transformers/all-mpnet-base-v2'
+        embeddings = create_embeddings(input_sample, model_name)
+        assert len(embeddings) == len(input_sample)
+
+    # TODO: actual tests
