b5fa842ab0f375945a493b4274686e5515ea7906
Hanna Reder <hanna.reder@usu.com>
2026-02-12 13:57:39 +0000

    reranking of dense and sparse hits with colbert

--- learn2rag/pipeline/search.py
+++ learn2rag/pipeline/search.py
@@ -1,6 +1,5 @@
 from .qdrant import Qdrant
 from .embeddings import create_embeddings
-import warnings
 from qdrant_client import models
 import numpy as np
 
@@ -23,7 +22,7 @@ def search(query, user_config, opt_config) -> list:
     if opt_config["embedding_model"] == "BAAI/bge-m3":
         if opt_config["search_mode"] == "dense_sparse":
             query_embedding = create_embeddings(query, opt_config["embedding_model"], embedding_mode="dense_sparse")
-        if opt_config["search_mode"] == "dense_sparse_colbert":
+        if opt_config["search_mode"] == "dense_sparse_colbert" or opt_config["search_mode"] == "reranking_with_colbert":
             query_embedding = create_embeddings(query, opt_config["embedding_model"], embedding_mode="dense_sparse_colbert")
         if opt_config["search_mode"] == "dense":
             query_embedding = create_embeddings(query, opt_config["embedding_model"], embedding_mode="dense")["dense_vecs"]
@@ -38,8 +37,8 @@ def search(query, user_config, opt_config) -> list:
     if opt_config["search_mode"] == "dense":
         results = qdrant.client.query_points(
             collection_name=collection_name,
-            query = query_embedding,
-            using = "dense",
+            query=query_embedding,
+            using="dense",
             limit=opt_config["top_k"],
         )
 
@@ -93,8 +92,31 @@ def search(query, user_config, opt_config) -> list:
     elif opt_config["search_mode"] == "multi_search":
         results = qdrant.client.query_points(
             collection_name=collection_name,
-            query = query_embedding,
-            using = "multi",
+            query=query_embedding,
+            using="multi",
+            limit=opt_config["top_k"],
+        )
+
+
+    elif opt_config["search_mode"] == "reranking_with_colbert":
+        indices = [int(k) for k in query_embedding["lexical_weights"].keys()]
+        values = [float(v) for v in query_embedding["lexical_weights"].values()]
+        results = qdrant.client.query_points(
+            collection_name=collection_name,
+            prefetch=[
+                models.Prefetch(
+                    query=models.SparseVector(indices=indices, values=values),
+                    using="sparse",
+                    limit=opt_config["prefetch_limit_sparse"],
+                ),
+                models.Prefetch(
+                    query=query_embedding["dense_vecs"],
+                    using="dense",
+                    limit=opt_config["prefetch_limit_dense"],
+                )
+            ],
+            query=list(query_embedding["colbert_vecs"]),
+            using="colbert",
             limit=opt_config["top_k"],
         )
 
