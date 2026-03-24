6495da8767a7986aaea7532c875eb4fc19c76080
denkv <denkv@users.noreply.github.com>
2026-03-24 12:50:56 +0100

    Handle exceptions in pipeline app wrapper to send at least some response

--- learn2rag/pipeline/app.py
+++ learn2rag/pipeline/app.py
@@ -88,48 +88,55 @@ async def stream(
 ) -> StreamingResponse:
     async def event_stream() -> AsyncGenerator[Any, Any]:
         request_id = secrets.token_hex()
-        question = inputs.messages[-1].content
+        try:
+            question = inputs.messages[-1].content
 
-        if not inputs.user:
-            raise ValueError("User Missing")
+            if not inputs.user:
+                raise ValueError("User Missing")
 
-        results = await search_authorized(user=inputs.user, question=question, request_id=request_id)
-        # sources = "\n".join(set(result.payload['path'] for result in results))
+            results = await search_authorized(user=inputs.user, question=question, request_id=request_id)
+            # sources = "\n".join(set(result.payload['path'] for result in results))
 
-        executor = ThreadPoolExecutor()
-        loop = asyncio.get_event_loop()
+            executor = ThreadPoolExecutor()
+            loop = asyncio.get_event_loop()
 
-        def sync_gen() -> Generator[str, Any, None]:
-            for chunk in generate.generate_stream(question, results, opt_config, request_id=request_id):
-                yield chunk
+            def sync_gen() -> Generator[str, Any, None]:
+                for chunk in generate.generate_stream(question, results, opt_config, request_id=request_id):
+                    yield chunk
 
-        chunks = await loop.run_in_executor(executor, lambda: list(sync_gen()))
+            chunks = await loop.run_in_executor(executor, lambda: list(sync_gen()))
 
-        yield f"data: {json.dumps({'choices': [{'delta': {}, 'finish_reason': None}]})}\n\n"
+            yield f"data: {json.dumps({'choices': [{'delta': {}, 'finish_reason': None}]})}\n\n"
 
-        for chunk in chunks:
-            msg = {
-                "choices": [
-                    {
-                        "delta": {"content": chunk},
-                        "finish_reason": None
-                    }
-                ]
-            }
-            yield f"data: {json.dumps(msg)}\n\n"
-            # await asyncio.sleep(0.1) # delay for stream check
+            for chunk in chunks:
+                msg = {
+                    "choices": [
+                        {
+                            "delta": {"content": chunk},
+                            "finish_reason": None
+                        }
+                    ]
+                }
+                yield f"data: {json.dumps(msg)}\n\n"
+                # await asyncio.sleep(0.1) # delay for stream check
 
-        # msg = {
-        #     "choices": [
-        #         {
-        #             "delta": {"content": "\n\n" + sources},
-        #             "finish_reason": None
-        #         }
-        #     ]
-        # }
-        # yield f"data: {json.dumps(msg)}\n\n"
+            # msg = {
+            #     "choices": [
+            #         {
+            #             "delta": {"content": "\n\n" + sources},
+            #             "finish_reason": None
+            #         }
+            #     ]
+            # }
+            # yield f"data: {json.dumps(msg)}\n\n"
 
-        yield f"data: {json.dumps({'choices': [{'delta': {}, 'finish_reason': 'stop'}]})}\n\n"
+            yield f"data: {json.dumps({'choices': [{'delta': {}, 'finish_reason': 'stop'}]})}\n\n"
+        except Exception as e:
+            logging.error('%s: %s', e.__class__, e)
+            content = 'There is a problem with Learn2RAG configuration. Please contact your administrator.'  # FIXME
+            delta = {'content': content}
+            yield f"data: {json.dumps({'choices': [{'delta': delta, 'finish_reason': 'stop'}]})}\n\n"
+
 
     return StreamingResponse(
         event_stream(),
