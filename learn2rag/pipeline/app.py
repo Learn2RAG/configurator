cceb299d414504f3093b7b674a27ac60b554be2c
denkv <denkv@users.noreply.github.com>
2026-03-31 11:21:28 +0200

    Add OpenAI-compatible API directly to our pipeline app

--- learn2rag/pipeline/app.py
+++ learn2rag/pipeline/app.py
@@ -3,7 +3,7 @@ import json
 import logging
 import secrets
 from concurrent.futures import ThreadPoolExecutor
-from typing import List, TypedDict, Generator, Any, AsyncGenerator
+from typing import Any, AsyncGenerator, Generator, List, Optional
 
 from fastapi import FastAPI, Body, Request, status
 from fastapi.exceptions import RequestValidationError
@@ -29,7 +29,8 @@ class Message(BaseModel):
 
 class ChatState(BaseModel):
     messages: List[Message]
-    user: str | None = None  # FIXME
+    stream: Optional[bool] = False
+    user: str | None = 'anonymous'  # FIXME use https://developers.openai.com/api/docs/guides/safety-best-practices#safety-identifiers
 
 class TestResponse(BaseModel):
     message: str
@@ -79,67 +80,79 @@ async def qanda(
     return ChatState(messages=[Message(content=answer, role="model")])
 
 
+@app.get("/models")  # OpenAI API for Open WebUI
+async def get_models() -> JSONResponse: return JSONResponse([{"id": "Learn2RAG"}])
+
+
 @app.post("/stream")
+@app.post("/chat/completions")  # OpenAI API for Open WebUI
 async def stream(
         inputs: ChatState = Body(
             ...,
             example=example_messages
         )
 ) -> StreamingResponse:
-    async def event_stream() -> AsyncGenerator[Any, Any]:
-        request_id = secrets.token_hex()
-        try:
-            question = inputs.messages[-1].content
-
-            if not inputs.user:
-                raise ValueError("User Missing")
-
-            results = await search_authorized(user=inputs.user, question=question, request_id=request_id)
-            # sources = "\n".join(set(result.payload['path'] for result in results))
-
-            executor = ThreadPoolExecutor()
-            loop = asyncio.get_event_loop()
-
-            def sync_gen() -> Generator[str, Any, None]:
-                for chunk in generate.generate_stream(question, results, opt_config, request_id=request_id):
-                    yield chunk
-
-            chunks = await loop.run_in_executor(executor, lambda: list(sync_gen()))
-
-            yield f"data: {json.dumps({'choices': [{'delta': {}, 'finish_reason': None}]})}\n\n"
-
-            for chunk in chunks:
-                msg = {
-                    "choices": [
-                        {
-                            "delta": {"content": chunk},
-                            "finish_reason": None
-                        }
-                    ]
-                }
-                yield f"data: {json.dumps(msg)}\n\n"
-                # await asyncio.sleep(0.1) # delay for stream check
-
-            # msg = {
-            #     "choices": [
-            #         {
-            #             "delta": {"content": "\n\n" + sources},
-            #             "finish_reason": None
-            #         }
-            #     ]
-            # }
-            # yield f"data: {json.dumps(msg)}\n\n"
-
-            yield f"data: {json.dumps({'choices': [{'delta': {}, 'finish_reason': 'stop'}]})}\n\n"
-        except Exception as e:
-            logging.error('%s: %s', e.__class__, e)
-            content = 'There is a problem with Learn2RAG configuration. Please contact your administrator.'  # FIXME
-            delta = {'content': content}
-            yield f"data: {json.dumps({'choices': [{'delta': delta, 'finish_reason': 'stop'}]})}\n\n"
+    if inputs.stream:
+        return streaming_response(inputs)
+    else:
+        raise NotImplementedError()
 
 
+async def event_stream(inputs: ChatState) -> AsyncGenerator[Any, Any]:
+    request_id = secrets.token_hex()
+    try:
+        question = inputs.messages[-1].content
+
+        if not inputs.user:
+            raise ValueError("User Missing")
+
+        results = await search_authorized(user=inputs.user, question=question, request_id=request_id)
+        # sources = "\n".join(set(result.payload['path'] for result in results))
+
+        executor = ThreadPoolExecutor()
+        loop = asyncio.get_event_loop()
+
+        def sync_gen() -> Generator[str, Any, None]:
+            for chunk in generate.generate_stream(question, results, opt_config, request_id=request_id):
+                yield chunk
+
+        chunks = await loop.run_in_executor(executor, lambda: list(sync_gen()))
+
+        yield f"data: {json.dumps({'choices': [{'delta': {}, 'finish_reason': None}]})}\n\n"
+
+        for chunk in chunks:
+            msg = {
+                "choices": [
+                    {
+                        "delta": {"content": chunk},
+                        "finish_reason": None
+                    }
+                ]
+            }
+            yield f"data: {json.dumps(msg)}\n\n"
+            # await asyncio.sleep(0.1) # delay for stream check
+
+        # msg = {
+        #     "choices": [
+        #         {
+        #             "delta": {"content": "\n\n" + sources},
+        #             "finish_reason": None
+        #         }
+        #     ]
+        # }
+        # yield f"data: {json.dumps(msg)}\n\n"
+
+        yield f"data: {json.dumps({'choices': [{'delta': {}, 'finish_reason': 'stop'}]})}\n\n"
+    except Exception as e:
+        logging.error('%s: %s', e.__class__, e)
+        content = 'There is a problem with Learn2RAG configuration. Please contact your administrator.'  # FIXME
+        delta = {'content': content}
+        yield f"data: {json.dumps({'choices': [{'delta': delta, 'finish_reason': 'stop'}]})}\n\n"
+
+
+def streaming_response(inputs: ChatState) -> StreamingResponse:
     return StreamingResponse(
-        event_stream(),
+        event_stream(inputs),
         media_type="text/event-stream",
         headers={"Cache-Control": "no-cache", "Connection": "keep-alive"}
     )
