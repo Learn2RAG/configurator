e0cf5894ca053c89381dfc44553467f0746e0950
denkv <denkv@users.noreply.github.com>
2026-06-02 16:08:56 +0200

    Add a mock LLM client for tests

--- learn2rag/pipeline/llm.py
+++ learn2rag/pipeline/llm.py
@@ -2,8 +2,11 @@ import logging
 import os
 from pydantic import SecretStr
 from langchain_core.language_models.chat_models import BaseChatModel
+from langchain_core.messages import AIMessage, BaseMessage, SystemMessage
+from langchain_core.outputs import ChatGeneration, ChatResult
 from langchain_ollama import ChatOllama
 from langchain_openai import ChatOpenAI
+from typing import Any, ClassVar
 
 
 logger = logging.getLogger(__name__)
@@ -23,6 +26,9 @@ class LLMClient():
 
 
 llms = {}
+'''A dict holding supported LLM client classes'''
+
+
 def llm_client(cls: type[LLMClient]) -> type[LLMClient]:
     llms[cls.ID] = cls; return cls
 
@@ -30,6 +36,7 @@ def llm_client(cls: type[LLMClient]) -> type[LLMClient]:
 # First @llm_client would be the default in UI when adding an external model
 @llm_client
 class OpenAIClient(LLMClient):
+    '''A LLM client based on OpenAI API'''
     ID = 'ChatOpenAI'
     LABEL = 'OpenAI'
 
@@ -44,6 +51,7 @@ class OpenAIClient(LLMClient):
 
 @llm_client
 class OllamaClient(LLMClient):
+    '''A LLM client based on Ollama API'''
     ID = 'ChatOllama'
     LABEL = 'Ollama'
 
@@ -59,7 +67,44 @@ class OllamaClient(LLMClient):
         )
 
 
+class TestFakeChatModel(BaseChatModel):
+    '''
+    A mock BaseChatModel implementation.
+    Responds with the full content of the system prompt.
+    '''
+    hint: ClassVar[str] = 'This is an internal model used for testing only.'
+
+    @property
+    def _llm_type(self) -> str: return 'test_fake_chat_model'
+
+    def _generate(
+            self,
+            messages: list[BaseMessage],
+            stop: list[str] | None = None,
+            run_manager: Any = None,
+            **kwargs: Any
+    ) -> ChatResult:
+        assert isinstance(messages[0], SystemMessage)
+        content = f'{self.hint} {messages[0].content}'
+        return ChatResult(
+            generations=[
+                ChatGeneration(message=AIMessage(content=content)),
+            ],
+        )
+
+
+@llm_client
+class FakeClient(LLMClient):
+    '''A mock LLM client to use only in tests'''
+    ID = 'ChatFake'
+    LABEL = None
+
+    def __init__(self, *, url: str, token: str | None, model: str, proxy: str | None) -> None:
+        self.chat_model = TestFakeChatModel()
+
+
 def chat_model_from_env() -> BaseChatModel:
+    '''Returns an instance of LLM client based on the environment variables'''
     default_llm = OpenAIClient
     llm_id = os.environ.get('LLM_API_TYPE', default_llm.ID)
     logger.debug('Using LLM: %s', llm_id)
