898815ed71785861a29fc42ef8a14cf3655fdb10
denkv <denkv@users.noreply.github.com>
2026-06-08 14:27:47 +0200

    Refactor qdrant initialization

--- learn2rag/pipeline/qdrant.py
+++ learn2rag/pipeline/qdrant.py
@@ -6,11 +6,14 @@ from qdrant_client.models import Distance, VectorParams, SparseVectorParams, Spa
 
 from .config import user_config
 
+api_key = os.environ.get('QDRANT__SERVICE__API_KEY')
+location = os.environ.get('QDRANT_LOCATION', 'http://localhost:6336')
+
 
 class Qdrant:
     client = QdrantClient(
-        location=os.environ.get('QDRANT_LOCATION', 'http://localhost:6336'),
-        api_key=os.environ.get('QDRANT__SERVICE__API_KEY'),
+        location=location,
+        api_key=api_key,
     )
 
     def __init__(self, collection_name: str, opt_config: dict[str, Any]) -> None:
