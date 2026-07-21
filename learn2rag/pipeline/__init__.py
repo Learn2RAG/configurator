df44158c9f071d89f3b7125c671309f352f67d3b
denkv <denkv@users.noreply.github.com>
2026-07-21 12:54:29 +0200

    Change user interface host

--- learn2rag/pipeline/__init__.py
+++ learn2rag/pipeline/__init__.py
@@ -7,6 +7,6 @@ def main() -> None:
     from .app import app
     uvicorn.run(
         app,
-        host="127.0.0.1",
+        host="0.0.0.0",  # FIXME: use learn2rag config value
         port=int(os.environ.get('LEARN2RAG_PIPELINE_PORT', 9000)),
     )
