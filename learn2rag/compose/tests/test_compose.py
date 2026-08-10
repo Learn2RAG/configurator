9df215fca86cb2aa97fe893c08f61a117a0ca40a
farshad68 <farshad.afshari68@gmail.com>
2026-08-10 13:51:20 +0200

    add windows version for compose tests

--- learn2rag/compose/tests/test_compose.py
+++ learn2rag/compose/tests/test_compose.py
@@ -1,12 +1,17 @@
 import pathlib
 import time
 import unittest
+import sys
 
 from .. import Project
 
 
 data_dir = pathlib.Path(__file__).parent.resolve() / 'data'
+is_windows = sys.platform == 'win32'
 
+def get_yaml_path(base_name: str) -> pathlib.Path:
+    filename = f"{base_name}_win.yml" if is_windows else f"{base_name}.yml"
+    return data_dir / filename
 
 class ComposeTestCase(unittest.TestCase):
     project: Project | None
@@ -20,7 +25,7 @@ class ComposeTestCase(unittest.TestCase):
 
     def test_zero_exitcode(self) -> None:
         name = 'test_zero_exitcode'
-        self.project = Project.create(data_dir / 'zero_exitcode.yml', name)
+        self.project = Project.create(get_yaml_path('zero_exitcode'), name)
         assert self.project is not None
         self.project.start()
         assert self.project.running
@@ -33,7 +38,7 @@ class ComposeTestCase(unittest.TestCase):
 
     def test_nonzero_exitcode(self) -> None:
         name = 'test_nonzero_exitcode'
-        self.project = Project.create(data_dir / 'nonzero_exitcode.yml', name)
+        self.project = Project.create(get_yaml_path('nonzero_exitcode'), name)
         assert self.project is not None
         self.project.start()
         assert self.project.running
@@ -46,7 +51,7 @@ class ComposeTestCase(unittest.TestCase):
 
     def test_stop(self) -> None:
         name = 'stop'
-        self.project = Project.create(data_dir / 'sleep.yml', name)
+        self.project = Project.create(get_yaml_path('sleep'), name)
         assert self.project is not None
         self.project.start()
         assert self.project.running
